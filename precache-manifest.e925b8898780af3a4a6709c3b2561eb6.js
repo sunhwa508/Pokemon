ce87eb57a3cc036dfd49a66c91d8d43069788c7f
sunhwa508 <2017sunhwa@gmail.com>
2020-05-05 19:28:06 +0900

    Updates

---
+++ precache-manifest.e925b8898780af3a4a6709c3b2561eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10351e6ce6e75b5317dc813b3868c470",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "7613b5a91dfb68ae17dc",
+    "url": "/Pokemon/static/css/main.1ee5edf4.chunk.css"
+  },
+  {
+    "revision": "80d0ec6ef05cce5f5b2b",
+    "url": "/Pokemon/static/js/2.055fcb12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon/static/js/2.055fcb12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7613b5a91dfb68ae17dc",
+    "url": "/Pokemon/static/js/main.3962ea9c.chunk.js"
+  },
+  {
+    "revision": "c903b0e79f6a73f8b99e",
+    "url": "/Pokemon/static/js/runtime-main.301a06a1.js"
+  }
+]);
